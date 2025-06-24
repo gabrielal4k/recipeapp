d496330ee5a91dbf5c9258cc52ca3c6635d26ef4
Gab <gabriel_al4k@yahoo.com>
2025-06-24 17:37:58 +0800

    api call fetch

--- recipeapp/src/components/Search.jsx
+++ recipeapp/src/components/Search.jsx
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from "react";
 
 export default function Search() {
   const [query, setQuery] = useState("");
+  const URL = "https://api.spoonacular.com/recipes/complexSearch";
+  const API_KEY = "f88265a0cd47446497613e3f91bb8964";
+
   useEffect(() => {
-    function demoRecipe() {
-      console.log("Demo recipe function called");
+    async function fetchRecipes() {
+      if (query) {
+        const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+        const data = await response.json();
+
+        console.log(data.results);
+      }
     }
-    demoRecipe();
+    fetchRecipes();
   }, [query]);
   return (
     <div>
